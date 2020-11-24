22f96fde8031a30392870e7c0ecf51fd5cd88308
eternalduck <eternalduckie@gmail.com>
2020-11-24 10:58:00 +0200

    added comments

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/style.scss';
-
 import App from './App';
 
-// const displayName = () => {
-//    console.log( 'Test' );
-//    const button = document.createElement( 'button' );
-//    button.textContent = 'Click me';
-//    document.body.appendChild( button )
-// };
-// displayName();
+// import './scss/style.scss';//FAIL
+//ERROR in ./src/scss/style.scss
+// Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):
+// ReferenceError: document is not defined
 
-const title = 'React!!!';
+// works
+const displayName = () => {
+   console.log( 'Test' );
+   const button = document.createElement( 'button' );
+   button.textContent = 'Click me..';
+   document.body.appendChild( button )
+};
+displayName();
 
+//fails
+const title = 'React...';
 ReactDOM.render(
   <App title={title} />,
   document.getElementById('root')
